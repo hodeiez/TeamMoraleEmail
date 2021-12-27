0349346427bb4fac48b026dcc9b7dfa458c7b87b
hodeiez <hodeinaiz@hotmail.com>
2021-12-27 22:01:58 +0100

    Initial commit

---
+++ src/main/java/hodei/naiz/teammoraleemail/TeamMoraleEmailApplication.java
@@ -0,0 +1,13 @@
+package hodei.naiz.teammoraleemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMoraleEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMoraleEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hodei/naiz/teammoraleemail/TeamMoraleEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package hodei.naiz.teammoraleemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMoraleEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
